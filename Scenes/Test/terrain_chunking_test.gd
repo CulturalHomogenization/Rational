c448778bb5a640d11031487ac9a4d7a53b896e1a
CulturalHomogenization <rayyan04@kingshigh.school.nz>
2025-07-25 10:57:31 +1200

    things

--- Scenes/Test/terrain_chunking_test.gd
+++ Scenes/Test/terrain_chunking_test.gd
@@ -1,13 +1,16 @@
 extends Node3D
 
-var terrain_chunk := preload("res://Scenes/scatter_chunk.tscn")
-var chunk_size := 50
-const terrain_size := 10
+const TERRAIN_CHUNK := preload("res://Scenes/scatter_chunk.tscn")
+const CHUNK_SIZE := 50
+const TERRAIN_SIZE := 10
+var proton_scatter : Node3D
 
 func _ready():
-	for i in range(terrain_size):
-		for i in range(terrain_size):
-			var proton_scatter = terrain_chunk.instantiate()
-			proton_scatter.position.x = i * chunk_size
-			add_child(proton_scatter)
-			await proton_scatter.build_completed
+	for j in range(TERRAIN_SIZE):
+		for i in range(TERRAIN_SIZE):
+			var chunk := TERRAIN_CHUNK.instantiate()
+			add_child(chunk)
+			chunk.position = Vector3(i * CHUNK_SIZE, 0, j * CHUNK_SIZE)
+			if chunk.has_signal("build_completed"):
+				await chunk.build_completed
+			print("Chunk placed:", i, j)
