2fd779f965ab69524b3b20f2551ceafc48201466
CulturalHomogenization <144862226+CulturalHomogenization@users.noreply.github.com>
2025-08-18 15:07:42 +1200

    gppd

--- Scenes/PlayerStats.gd
+++ Scenes/PlayerStats.gd
@@ -1,9 +1,10 @@
-extends Node
+extends Resource
+class_name PlayerStats
 
-var growing_speed := 60
-var cooking_speed := 5
-var harvesting_speed := 20
-var sowing_speed := 10
-var yield_amount := 40
+@export var growing_speed := 60
+@export var cooking_speed := 5
+@export var harvesting_speed := 20
+@export var sowing_speed := 10
+@export var yield_amount := 40
 
-var upgrades : Array = []
+@export var upgrades : Array = []
